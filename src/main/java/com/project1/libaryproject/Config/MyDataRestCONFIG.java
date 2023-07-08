ad6674a34a498a8c924d065746b1e8c505989a7b
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-08 06:15:39 +0100

    Start

--- src/main/java/com/project1/libaryproject/Config/MyDataRestCONFIG.java
+++ src/main/java/com/project1/libaryproject/Config/MyDataRestCONFIG.java
@@ -1,4 +1,20 @@
 package com.project1.libaryproject.Config;
 
-public class MyDataRestCONFIG {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class MyDataRestCONFIG implements RepositoryRestConfigurer {
+
+private String theAllowedOrgin = "http://localhost:3000";
+public void configureRepositoryRestConfiguration(RepositoryRestConfigurer config, CorsRegistry registry){
+    HttpMethod[] theUnsupportedActions = {HttpMethod.POST, HttpMethod.PATCH, HttpMethod.DELETE,HttpMethod.PUT};
+
+}
+
+
+
+
 }
