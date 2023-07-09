94c0d89be85c113e38fd1c3a2b3865a0d7e997a8
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-09 09:54:41 +0100

    Start

--- src/main/java/com/project1/libaryproject/DAO/BookRepository.java
+++ src/main/java/com/project1/libaryproject/DAO/BookRepository.java
@@ -1,7 +1,14 @@
 package com.project1.libaryproject.DAO;
 
 import com.project1.libaryproject.Entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+    //Since Jpa has no Search option we will create a method to search for books by title
+    //Which will generate the method at runtime
+
+    Page<Book> findByTitleContaining(@Param("title") String title, Pageable pageable);
 }
