c7a5f7c21049f655da23d87a92c1452c2d5c6d5f
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-10 06:35:30 +0100

    Start

--- src/main/java/com/project1/libaryproject/Entity/Checkout.java
+++ src/main/java/com/project1/libaryproject/Entity/Checkout.java
@@ -2,11 +2,15 @@ package com.project1.libaryproject.Entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "checkout")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Checkout {
 
 
