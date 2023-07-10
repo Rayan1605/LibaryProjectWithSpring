facdb9ef9e24fadf6931cd79c9c9c67829083ea8
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-10 05:58:01 +0100

    Start

--- src/main/java/com/project1/libaryproject/Entity/Review.java
+++ src/main/java/com/project1/libaryproject/Entity/Review.java
@@ -1,4 +1,12 @@
 package com.project1.libaryproject.Entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "review")
+@Data
+
 public class Review {
 }
