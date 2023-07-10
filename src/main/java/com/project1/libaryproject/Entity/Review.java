ae41d81d4f46721e5df2577b22b17030c43e9fae
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-10 06:01:03 +0100

    Start

--- src/main/java/com/project1/libaryproject/Entity/Review.java
+++ src/main/java/com/project1/libaryproject/Entity/Review.java
@@ -1,12 +1,41 @@
 package com.project1.libaryproject.Entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.util.Date;
 
 @Entity
 @Table(name = "review")
 @Data
 
 public class Review {
+    @Id
+    @GeneratedValue()
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "user_email")
+    private String user_email;
+
+    @Column(name = "date")
+ @CreationTimestamp
+    private Date date;
+
+@Column(name = "rating")
+    private double rating;
+@Column(name = "book_id")
+private Long book_id;
+
+    @Column(name = "review_description")
+private String reviewDecription;
+
+
+
+
+
+
+
+
 }
