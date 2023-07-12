bd16aeaee74ebe5f9d7319e453168b4b8a4333fb
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-13 00:20:34 +0100

    Start

--- src/main/java/com/project1/libaryproject/Service/BookService.java
+++ src/main/java/com/project1/libaryproject/Service/BookService.java
@@ -3,16 +3,21 @@ package com.project1.libaryproject.Service;
 import com.project1.libaryproject.DAO.BookRepository;
 import com.project1.libaryproject.DAO.CheckOutRepository;
 import com.project1.libaryproject.Entity.Checkout;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
+@AllArgsConstructor
 public class BookService {
 
 private BookRepository bookRepository;
-
+//Using lomback to generate constructor for dependency injection
 private CheckOutRepository checkout;
+
+
 }
 
 
