1dec75e653d853c21eb3ffdf1ad0188d873ec077
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-13 00:19:05 +0100

    Start

--- src/main/java/com/project1/libaryproject/Service/BookService.java
+++ src/main/java/com/project1/libaryproject/Service/BookService.java
@@ -1,4 +1,32 @@
 package com.project1.libaryproject.Service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
 public class BookService {
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
