88a778953dfef538a96b00d01587931a53bf9df9
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-13 00:22:28 +0100

    Start

--- src/main/java/com/project1/libaryproject/Service/BookService.java
+++ src/main/java/com/project1/libaryproject/Service/BookService.java
@@ -2,6 +2,7 @@ package com.project1.libaryproject.Service;
 
 import com.project1.libaryproject.DAO.BookRepository;
 import com.project1.libaryproject.DAO.CheckOutRepository;
+import com.project1.libaryproject.Entity.Book;
 import com.project1.libaryproject.Entity.Checkout;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
@@ -17,6 +18,14 @@ private BookRepository bookRepository;
 //Using lomback to generate constructor for dependency injection
 private CheckOutRepository checkout;
 
+public Book checkoutBook(String userEmail, Long bookId) throws Exception {
+//This method will checkout a book by a user
+//It will check if the user has already checked out the book
+//If the user has already checked out the book it will throw an exception
+//If the user has not checked out the book it will checkout the book and return the book
+
+
+}
 
 }
 
