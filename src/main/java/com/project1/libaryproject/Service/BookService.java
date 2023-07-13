5427b7e6d46773d4d543da037e62f753c32dad29
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-13 01:07:57 +0100

    Start

--- src/main/java/com/project1/libaryproject/Service/BookService.java
+++ src/main/java/com/project1/libaryproject/Service/BookService.java
@@ -5,7 +5,6 @@ import com.project1.libaryproject.DAO.CheckOutRepository;
 import com.project1.libaryproject.Entity.Book;
 import com.project1.libaryproject.Entity.Checkout;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -43,10 +42,10 @@ public class BookService {
             case 3 -> throw new Exception("Book is not available");
         }
         book.get().setAvailable_copies(book.get().getAvailable_copies() - 1);
-    bookRepository.save(book.get());
+        bookRepository.save(book.get());
 //If the book is found we will checkout the book
         Checkout checkout = new Checkout(
-          userEmail,
+                userEmail,
                 LocalDate.now().toString(),
                 LocalDate.now().plusDays(7).toString(),
                 book.get().getId()
@@ -57,7 +56,7 @@ public class BookService {
 
     }
 
-    private int Validate(Checkout validateUser,Optional<Book> book ) throws Exception{
+    private int Validate(Checkout validateUser,Optional<Book> book ) {
         if (validateUser != null) return 1;
         if (book.isEmpty()) return 2;
         if (book.get().getAvailable_copies() == 0) return 3;
