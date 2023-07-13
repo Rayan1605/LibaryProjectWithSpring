0d4bf3767692976c29454ef4e3f87be1011b2c51
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-14 00:50:10 +0100

    Fixing Errors

--- src/main/java/com/project1/libaryproject/Utils/ExtractJwt.java
+++ src/main/java/com/project1/libaryproject/Utils/ExtractJwt.java
@@ -1,5 +1,7 @@
 package com.project1.libaryproject.Utils;
 
+import java.util.Base64;
+
 public class ExtractJwt {
 //This is because the token starts with Bearer and then the token so we need to take it off
     public static String extractJwtToken(String header) {
@@ -8,6 +10,8 @@ public class ExtractJwt {
         // the first part is the header, the second part is the payload, and the third part is the signature
         // the // is to escape the "."
         // because it's a special character
+
+        Base64.Decoder decoder = Base64.getUrlDecoder();
 
 
 
