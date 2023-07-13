e3075f971c74f01a660112fe0e57efbdc53a61cc
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-14 00:49:37 +0100

    Fixing Errors

--- src/main/java/com/project1/libaryproject/Utils/ExtractJwt.java
+++ src/main/java/com/project1/libaryproject/Utils/ExtractJwt.java
@@ -4,7 +4,11 @@ public class ExtractJwt {
 //This is because the token starts with Bearer and then the token so we need to take it off
     public static String extractJwtToken(String header) {
          header.replace("Bearer", "").trim();
-    String[] parts = header.split("\\.");
+    String[] parts = header.split("\\.");//This is to split the token into 3 parts
+        // the first part is the header, the second part is the payload, and the third part is the signature
+        // the // is to escape the "."
+        // because it's a special character
+
 
 
     }
