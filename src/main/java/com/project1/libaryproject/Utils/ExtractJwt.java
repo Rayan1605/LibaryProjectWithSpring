19054ff2b1337241364eca650d4d7e60ebb390bc
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-14 01:25:17 +0100

    Making it more dynamic

--- src/main/java/com/project1/libaryproject/Utils/ExtractJwt.java
+++ src/main/java/com/project1/libaryproject/Utils/ExtractJwt.java
@@ -20,10 +20,15 @@ public class ExtractJwt {
         //Above is written so we can get just the payload part of the token
         // And ignore the header and the signature
 
-        String[] entries = payload.split(",");
+        String[] entries = ExtractPayload(header).split(",");
+
         //So every element in the payload will be it own element in the array
 
         Map<String, String> map = new HashMap<>();// This is to store the key value pairs
+
+
+        //Now we are looking for the key of sub of extraction
+
 
         for (String entry : entries) {
             //We are looping through the entries array to find the value of sub
@@ -41,7 +46,6 @@ public class ExtractJwt {
                 keyValue[1] = keyValue[1].substring(1);
                 map.put(keyValue[0], keyValue[1]);
             }
-
         }
 if (map.containsKey(extraction)) { // if the map contains the key of sub then return the VALUR of
     // sub
@@ -51,6 +55,20 @@ if (map.containsKey(extraction)) { // if the map contains the key of sub then re
 
     }
 
+    private static String ExtractPayload(String header) {
+    header.
+        String[] parts = header.split("\\.");//This is to split the token into 3 parts
+        // the first part is the header, the second part is the payload, and the third part is the signature
+        // the // is to escape the "."
+        // because it's a special character
 
+        Base64.Decoder decoder = Base64.getUrlDecoder(); //This is to decode the token
+        //Above is written so we can get just the payload part of the token
+        // And ignore the header and the signature
+   return new String(decoder.decode(parts[1]));
+    }
 
 }
+
+
+
