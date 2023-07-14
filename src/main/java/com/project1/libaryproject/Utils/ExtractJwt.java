34b4c366402f0cc5bb2e05a766182525722b8e10
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-14 01:15:24 +0100

    Making it more dynamic

--- src/main/java/com/project1/libaryproject/Utils/ExtractJwt.java
+++ src/main/java/com/project1/libaryproject/Utils/ExtractJwt.java
@@ -7,6 +7,8 @@ import java.util.Map;
 public class ExtractJwt {
     //This is because the token starts with Bearer and then the token so we need to take it off
     public static String extractJwtToken(String header,String extraction) {
+        //extraction is the key we are looking for
+        //so can be the sub or the email
         header.replace("Bearer", "").trim();
         String[] parts = header.split("\\.");//This is to split the token into 3 parts
         // the first part is the header, the second part is the payload, and the third part is the signature
