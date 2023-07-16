8b0afff5d1d80a0563da5e875478b3b165815aba
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-16 15:17:28 +0100

    Making it more dynamic

--- src/main/java/com/project1/libaryproject/Service/ReviewService.java
+++ src/main/java/com/project1/libaryproject/Service/ReviewService.java
@@ -1,4 +1,10 @@
 package com.project1.libaryproject.Service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional //  commits the transaction after the method completes successfully.
+// If an exception is thrown, the transaction will be rolled back.
 public class ReviewService {
 }
