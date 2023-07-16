69935515dc4389288998ce8ed60e1bdccd6f953b
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-16 15:50:47 +0100

    Making it more dynamic

--- src/main/java/com/project1/libaryproject/Service/ReviewService.java
+++ src/main/java/com/project1/libaryproject/Service/ReviewService.java
@@ -44,7 +44,6 @@ public class ReviewService {
 
     public Boolean checkIfUserHasAlreadyLeftAReview(String userEmail, Long bookId) {
         Review review = reviewRepository.findByuserEmailAndBookId(userEmail, bookId);
-        System.out.println(review);
-      //  return review != null;
+        return review != null;
     }
 }
