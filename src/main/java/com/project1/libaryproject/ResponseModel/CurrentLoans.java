18e7824cc83a361ab15546a03c33d3c920950154
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-16 16:13:58 +0100

    Making it more dynamic

--- src/main/java/com/project1/libaryproject/ResponseModel/CurrentLoans.java
+++ src/main/java/com/project1/libaryproject/ResponseModel/CurrentLoans.java
@@ -1,9 +1,14 @@
 package com.project1.libaryproject.ResponseModel;
 
+import com.project1.libaryproject.Entity.Book;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class CurrentLoans {
 
+    private Book book;
+    int daysLeft;
 
 }
