8091537d5d75f39cf7351a75b7393b9d9cb6ceb1
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-16 15:15:14 +0100

    Making it more dynamic

--- src/main/java/com/project1/libaryproject/RequestModels/ReviewRequest.java
+++ src/main/java/com/project1/libaryproject/RequestModels/ReviewRequest.java
@@ -11,4 +11,5 @@ private double rating;
 private Optional<String> reviewDescription; //because this is not mandatory
 //We will use Optional since It is often used as a return type for methods that may return null.
 
+
 }
