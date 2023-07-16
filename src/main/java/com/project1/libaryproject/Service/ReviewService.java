2f3a6b4c3a43b7ab69d73f16c1c5772d32ddddb3
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-16 15:20:07 +0100

    Making it more dynamic

--- src/main/java/com/project1/libaryproject/Service/ReviewService.java
+++ src/main/java/com/project1/libaryproject/Service/ReviewService.java
@@ -2,6 +2,7 @@ package com.project1.libaryproject.Service;
 
 import com.project1.libaryproject.DAO.BookRepository;
 import com.project1.libaryproject.DAO.ReviewRepository;
+import com.project1.libaryproject.RequestModels.ReviewRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,5 +15,7 @@ public class ReviewService {
     private final ReviewRepository reviewRepository;
     private final BookRepository bookRepository;
 
-    public void postReview(String userEmail,)
+    public void postReview(String userEmail, ReviewRequest reviewRequest) throws Exception{
+
+    }
 }
