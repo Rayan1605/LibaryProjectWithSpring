51ea88359efc54b9d8dfccacafe54c77a7e4f1f0
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-16 15:47:12 +0100

    Making it more dynamic

--- src/main/java/com/project1/libaryproject/Controller/ReviewController.java
+++ src/main/java/com/project1/libaryproject/Controller/ReviewController.java
@@ -18,6 +18,8 @@ public class ReviewController {
     public void postReview(@RequestHeader(value = "Authorization") String token,
                            @RequestBody ReviewRequest reviewRequest) throws Exception {
      String userEmail = ExtractJwt.extractJwtExtraction(token, "\"sub\"");
+     if (userEmail == null) throw new Exception("You are not logged in");
+        reviewController.postReview(userEmail, reviewRequest);
 
     }
 }
