1e7c332c026a558c12d0d4cf746f6519362bed94
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-16 03:43:24 +0100

    Making it more dynamic

--- src/main/java/com/project1/libaryproject/RequestModels/ReviewRequest.java
+++ src/main/java/com/project1/libaryproject/RequestModels/ReviewRequest.java
@@ -2,8 +2,12 @@ package com.project1.libaryproject.RequestModels;
 
 import lombok.Data;
 
+import java.util.Optional;
+
 @Data
 public class ReviewRequest {
 private Long bookId;
+private double rating;
+private Optional
 
 }
