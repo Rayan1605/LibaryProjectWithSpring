d1149789a2474937266f3e72c456e1f5e70dea9f
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-16 15:18:02 +0100

    Making it more dynamic

--- src/main/java/com/project1/libaryproject/Service/ReviewService.java
+++ src/main/java/com/project1/libaryproject/Service/ReviewService.java
@@ -1,10 +1,17 @@
 package com.project1.libaryproject.Service;
 
+import com.project1.libaryproject.DAO.BookRepository;
+import com.project1.libaryproject.DAO.ReviewRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@AllArgsConstructor
 @Transactional //  commits the transaction after the method completes successfully.
 // If an exception is thrown, the transaction will be rolled back.
 public class ReviewService {
+
+    private final ReviewRepository reviewRepository;
+    private final BookRepository bookRepository;
 }
