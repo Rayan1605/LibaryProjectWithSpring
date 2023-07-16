f59f19421382feb0c9f8946e23d2409de0cba086
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-16 15:35:45 +0100

    Making it more dynamic

--- src/main/java/com/project1/libaryproject/Controller/ReviewController.java
+++ src/main/java/com/project1/libaryproject/Controller/ReviewController.java
@@ -1,4 +1,8 @@
 package com.project1.libaryproject.Controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = "http://localhost:3000")//This is to allow the React app
+// to access the api
 public class ReviewController {
 }
