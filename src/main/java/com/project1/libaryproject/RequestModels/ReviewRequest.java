3f6073ee8123bb1d70c81cf6b2add01936818109
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-16 15:12:19 +0100

    Making it more dynamic

--- src/main/java/com/project1/libaryproject/RequestModels/ReviewRequest.java
+++ src/main/java/com/project1/libaryproject/RequestModels/ReviewRequest.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 public class ReviewRequest {
 private Long bookId;
 private double rating;
-private Optional<String> reviewDescription;
+private Optional<String> reviewDescription; //because this is not mandoatory
+
 
 }
