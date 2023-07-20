c8498ee5f1378f50c47b63790e02543c6c6218d3
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-20 02:30:23 +0100

    Making it more dynamic

--- src/main/java/com/project1/libaryproject/Controller/MessageController.java
+++ src/main/java/com/project1/libaryproject/Controller/MessageController.java
@@ -1,4 +1,9 @@
 package com.project1.libaryproject.Controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:3000")//This is to allow the React app
+@RestController
 public class MessageController {
 }
