facd97b0d7003669a273ed81a8c6964c1e8d1d8b
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-20 02:46:32 +0100

    Making it more dynamic

--- src/main/java/com/project1/libaryproject/Entity/Histroy.java
+++ src/main/java/com/project1/libaryproject/Entity/Histroy.java
@@ -2,8 +2,19 @@ package com.project1.libaryproject.Entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "History")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Histroy {
+
+
+
+
+
 }
