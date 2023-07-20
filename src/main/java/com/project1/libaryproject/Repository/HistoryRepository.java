3943d45ec1ef72148f442c0fb2afc99de139a1a5
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-20 02:49:54 +0100

    Making it more dynamic

--- src/main/java/com/project1/libaryproject/Repository/HistoryRepository.java
+++ src/main/java/com/project1/libaryproject/Repository/HistoryRepository.java
@@ -1,7 +1,10 @@
 package com.project1.libaryproject.Repository;
 
 import com.project1.libaryproject.Entity.Histroy;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HistoryRepository extends JpaRepository<Histroy,Long> {
+
+    Page<Histroy> findBooksByUserEmail(@Param("user_email") String userEmail, Pageable pageable);
 }
