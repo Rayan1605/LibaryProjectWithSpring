aed2a61b99fce31ee0fbff0d32d33e939592cbf8
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-20 02:22:22 +0100

    Making it more dynamic

--- src/main/java/com/project1/libaryproject/Service/MessagesService.java
+++ src/main/java/com/project1/libaryproject/Service/MessagesService.java
@@ -1,11 +1,13 @@
 package com.project1.libaryproject.Service;
 
 import com.project1.libaryproject.Repository.MessageRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
+@AllArgsConstructor
 public class MessagesService {
     MessageRepository messageRepository;
 
