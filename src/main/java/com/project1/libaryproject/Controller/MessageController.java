b1c36734c69ca9a972f4e5a663d9bee299c9fe0f
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-20 02:34:29 +0100

    Making it more dynamic

--- src/main/java/com/project1/libaryproject/Controller/MessageController.java
+++ src/main/java/com/project1/libaryproject/Controller/MessageController.java
@@ -1,11 +1,28 @@
 package com.project1.libaryproject.Controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.project1.libaryproject.Entity.Message;
+import com.project1.libaryproject.Service.MessagesService;
+import com.project1.libaryproject.Utils.ExtractJwt;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "http://localhost:3000")//This is to allow the React app
 @RestController
 @RequestMapping("/api/messages")
+@AllArgsConstructor
 public class MessageController {
+
+    private MessagesService messagesService;
+
+    private String CheckJwt(String token) throws Exception{
+        String userEmail = ExtractJwt.extractJwtExtraction(token, "\"sub\"");
+        if (userEmail == null) {throw new Exception("You are not logged in");}
+        return userEmail;
+    }
+
+    @PostMapping("/secure/add/message")
+    public void postMessage(@RequestHeader(value = "Authorization") String token, @RequestBody Message messageRequest) throws Exception {
+        messagesService.addMessage(CheckJwt(token), message);
+    }
+
 }
