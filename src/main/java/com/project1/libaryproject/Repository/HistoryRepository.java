6806d0e192885d23a1774cde8c119dc14d71c657
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-20 02:50:08 +0100

    Making it more dynamic

--- src/main/java/com/project1/libaryproject/Repository/HistoryRepository.java
+++ src/main/java/com/project1/libaryproject/Repository/HistoryRepository.java
@@ -2,7 +2,9 @@ package com.project1.libaryproject.Repository;
 
 import com.project1.libaryproject.Entity.Histroy;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface HistoryRepository extends JpaRepository<Histroy,Long> {
 
