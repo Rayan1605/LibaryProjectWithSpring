9e304c99fbd762ecc3d97663642dcd4d82c8ebd7
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-20 02:09:12 +0100

    Making it more dynamic

---
+++ src/main/java/com/project1/libaryproject/Entity/Messsage.java
@@ -0,0 +1,21 @@
+package com.project1.libaryproject.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "messages")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Messsage {
+@Id
+@GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+@Column(name = "id")
+private Long id;
+
+
+
+}
