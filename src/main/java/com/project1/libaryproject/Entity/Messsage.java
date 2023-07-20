2c5d6da8f05f2ddd839d13db9700bf6de2103556
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-20 02:12:19 +0100

    Making it more dynamic

--- src/main/java/com/project1/libaryproject/Entity/Messsage.java
+++ src/main/java/com/project1/libaryproject/Entity/Messsage.java
@@ -15,16 +15,22 @@ public class Messsage {
 @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
 @Column(name = "id")
 private Long id;
+
 @Column(name = "user_email")
 private String userEmail;
+
 @Column(name = "title")
 private String title;
+
 @Column(name = "question")
 private String question;
+
 @Column(name = "admin_email")
 private String adminEmail;
+
 @Column(name = "response")
 private String response;
+
 @Column(name = "closed")
 private boolean closed;
 
