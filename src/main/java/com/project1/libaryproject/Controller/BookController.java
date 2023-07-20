be68ac95ca3776ea06deb7ef27b7a0ac0d67b48a
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-20 01:23:58 +0100

    Making it more dynamic

--- src/main/java/com/project1/libaryproject/Controller/BookController.java
+++ src/main/java/com/project1/libaryproject/Controller/BookController.java
@@ -80,9 +80,7 @@ public class BookController {
         bookService.returnBook(userEmail, bookId);//then returning the book
     }
 
-    public void RenewLoan(String userEmail, Long bookId) throws Exception {
-        bookService.renewLoan(userEmail, bookId);
-    }
+
 
 }
 
