d7e2c161db34377b8ee1e4a44730edf7379794b7
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-20 02:48:09 +0100

    Making it more dynamic

--- src/main/java/com/project1/libaryproject/Entity/Histroy.java
+++ src/main/java/com/project1/libaryproject/Entity/Histroy.java
@@ -19,6 +19,25 @@ public class Histroy {
     @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name="user_email")
+    private String userEmail;
 
+    @Column(name="checkout_date")
+    private String checkoutDate;
+
+    @Column(name="returned_date")
+    private String returnedDate;
+
+    @Column(name="title")
+    private String title;
+
+    @Column(name="author")
+    private String author;
+
+    @Column(name="description")
+    private String description;
+
+    @Column(name="img")
+    private String img
 
 }
