f02ab5481392584444c1107ac145aace76b28398
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-20 02:39:37 +0100

    Making it more dynamic

--- src/main/java/com/project1/libaryproject/Repository/MessageRepository.java
+++ src/main/java/com/project1/libaryproject/Repository/MessageRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
+//This will find all message by user email and return a page of messages
 
     Page<Message> findByUserEmail(String userEmail, Pageable pageable);
 }
