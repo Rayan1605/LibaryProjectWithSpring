b98b76e2bcb8c883dffd297845f87159e9527ebf
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-20 02:14:46 +0100

    Making it more dynamic

--- src/main/java/com/project1/libaryproject/Repository/MessageRepository.java
+++ src/main/java/com/project1/libaryproject/Repository/MessageRepository.java
@@ -3,5 +3,5 @@ package com.project1.libaryproject.Repository;
 import com.project1.libaryproject.Entity.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public class MessageRepository extends JpaRepository<Message, Long> {
+public interface MessageRepository extends JpaRepository<Message, Long> {
 }
