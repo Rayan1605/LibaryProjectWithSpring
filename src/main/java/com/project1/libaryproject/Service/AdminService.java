2047d877e4a5e1426323909bd15c1b3cf9ba6eaf
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-25 20:04:25 +0100

    Making it more dynamic

--- src/main/java/com/project1/libaryproject/Service/AdminService.java
+++ src/main/java/com/project1/libaryproject/Service/AdminService.java
@@ -1,6 +1,8 @@
 package com.project1.libaryproject.Service;
 
+import com.project1.libaryproject.Entity.Book;
 import com.project1.libaryproject.Repository.BookRepository;
+import com.project1.libaryproject.RequestModels.AddBookRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,7 +14,9 @@ public class AdminService {
 
     private BookRepository bookRepository;
 
-public void postBook()
+public void postBook(AddBookRequest addBookRequest) {
+    Book book = new Book();
+}
 
 
 }
