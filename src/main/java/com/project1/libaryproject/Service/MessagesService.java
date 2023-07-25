b29ee80ca4243d701d2c05ff5153d8fa3d5b53a5
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-25 19:43:26 +0100

    Making it more dynamic

--- src/main/java/com/project1/libaryproject/Service/MessagesService.java
+++ src/main/java/com/project1/libaryproject/Service/MessagesService.java
@@ -2,9 +2,12 @@ package com.project1.libaryproject.Service;
 
 import com.project1.libaryproject.Entity.Message;
 import com.project1.libaryproject.Repository.MessageRepository;
+import com.project1.libaryproject.RequestModels.AdminQuestionRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -15,6 +18,14 @@ public class MessagesService {
     public void postMessage( String userEmail,Message message) {
     message.setUserEmail(userEmail);
     messageRepository.save(message);
+    }
+
+    public void putMessage(AdminQuestionRequest adminQuestionRequest,String userEmail) {
+        //Below is searching for a message that contain this id
+        Optional<Message>  message = messageRepository.findById(adminQuestionRequest.getId());
+
+        if (message.isEmpty()) throw new IllegalStateException("Message not found");
+
     }
 
 }
