c96f4d1e01b8b3619732ba0ad234603386db2dfa
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-25 20:45:28 +0100

    Making it more dynamic

--- src/main/java/com/project1/libaryproject/Controller/AdminController.java
+++ src/main/java/com/project1/libaryproject/Controller/AdminController.java
@@ -4,7 +4,6 @@ import com.project1.libaryproject.RequestModels.AddBookRequest;
 import com.project1.libaryproject.Service.AdminService;
 import com.project1.libaryproject.Utils.ExtractJwt;
 import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "http://localhost:3000")//This is to allow the React app
@@ -17,26 +16,26 @@ public class AdminController {
     @PostMapping("/secure/add/book")
     public void postBook(@RequestHeader (value = "Authorization")String token ,
                          @RequestBody AddBookRequest addBookRequest) throws Exception {
-        if (!CheckIfAdmin(token))throw new Exception("You are not an admin");
+        if (CheckIfAdmin(token))throw new Exception("You are not an admin");
         adminService.postBook(addBookRequest);
 
     }
     @PostMapping("/secure/increase/book/quantity")
     public void addQuantity(@RequestHeader (value = "Authorization")String token ,
                             @RequestParam Long bookId) throws Exception {
-        if (!CheckIfAdmin(token))throw new Exception("You are not an admin");
+        if (CheckIfAdmin(token))throw new Exception("You are not an admin");
       adminService.IncreaseBookQuantity(bookId);
 
     }
     @PostMapping("/secure/decrease/book/quantity")
     public void decreaseQuantity(@RequestHeader (value = "Authorization")String token ,
                             @RequestParam Long bookId) throws Exception {
-        if (!CheckIfAdmin(token))throw new Exception("You are not an admin");
+        if (CheckIfAdmin(token))throw new Exception("You are not an admin");
        adminService.DecreaseBookQuantity(bookId);
 
     }
     private boolean CheckIfAdmin(String token) {
         String admin = ExtractJwt.extractJwtExtraction(token, "\"userType\"");
-        return admin != null && admin.equals("admin");
+        return admin == null || admin.equals("admin");
     }
 }
