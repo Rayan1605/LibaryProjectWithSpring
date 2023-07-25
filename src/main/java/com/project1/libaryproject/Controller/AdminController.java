94267d0fbbb923d0cb01b6a50b14dc7e67c0fff6
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-25 20:15:01 +0100

    Making it more dynamic

--- src/main/java/com/project1/libaryproject/Controller/AdminController.java
+++ src/main/java/com/project1/libaryproject/Controller/AdminController.java
@@ -1,10 +1,9 @@
 package com.project1.libaryproject.Controller;
 
+import com.project1.libaryproject.RequestModels.AddBookRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "http://localhost:3000")//This is to allow the React app
 @AllArgsConstructor
@@ -13,5 +12,12 @@ public class AdminController {
 
     private AdminController adminController;
 
+    @PostMapping("/secure/add/book")
+    public void postBook(@RequestHeader (value = "Authorization")String token ,
+                         @RequestBody AddBookRequest addBookRequest) throws Exception {
+
+        adminController.postBook();
+
+    }
 
 }
