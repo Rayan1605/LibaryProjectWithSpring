6e22c309382891145d56b1cadcf30cda1aafbb13
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-25 20:15:14 +0100

    Making it more dynamic

--- src/main/java/com/project1/libaryproject/Controller/AdminController.java
+++ src/main/java/com/project1/libaryproject/Controller/AdminController.java
@@ -1,6 +1,7 @@
 package com.project1.libaryproject.Controller;
 
 import com.project1.libaryproject.RequestModels.AddBookRequest;
+import com.project1.libaryproject.Utils.ExtractJwt;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -19,5 +20,8 @@ public class AdminController {
         adminController.postBook();
 
     }
-
+    private boolean CheckIfAdmin(String token) {
+        String admin = ExtractJwt.extractJwtExtraction(token, "\"userType\"");
+        return admin != null && admin.equals("admin");
+    }
 }
