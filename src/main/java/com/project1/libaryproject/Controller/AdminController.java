49dc9764ae39a836f4380383c4c0f862e84f9b3b
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-25 20:13:28 +0100

    Making it more dynamic

--- src/main/java/com/project1/libaryproject/Controller/AdminController.java
+++ src/main/java/com/project1/libaryproject/Controller/AdminController.java
@@ -1,12 +1,15 @@
 package com.project1.libaryproject.Controller;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "http://localhost:3000")//This is to allow the React app
-@
+@AllArgsConstructor
 @RequestMapping("/api/admin")
 public class AdminController {
+
+    private
 }
