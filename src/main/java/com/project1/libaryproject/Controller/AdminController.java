87d91455c2ce1816d3bb4a0c7fcb35b1ae1a22c6
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-25 20:12:51 +0100

    Making it more dynamic

---
+++ src/main/java/com/project1/libaryproject/Controller/AdminController.java
@@ -0,0 +1,10 @@
+package com.project1.libaryproject.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@Controller
+@CrossOrigin(origins = "http://localhost:3000")//This is to allow the React app
+
+public class AdminController {
+}
