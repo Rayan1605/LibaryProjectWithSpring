029c78a23b7abaed439e991afe47d1bd22ab0118
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-25 19:36:51 +0100

    Making it more dynamic

--- src/main/java/com/project1/libaryproject/Repository/MessageRepository.java
+++ src/main/java/com/project1/libaryproject/Repository/MessageRepository.java
@@ -10,4 +10,9 @@ public interface MessageRepository extends JpaRepository<Message, Long> {
 //This will find all messages by user email and return a page of messages
 
     Page<Message> findByUserEmail(@RequestParam("user_email") String userEmail, Pageable pageable);
+//This is going to be the endpoint to find all messages by closed status
+// and return a page of messages
+    //So all user who have closed messages the admin can see them and that what this is going to return
+    Page<Message> findbyClosed(@RequestParam("closed") boolean closed, Pageable pageable);
+
 }
