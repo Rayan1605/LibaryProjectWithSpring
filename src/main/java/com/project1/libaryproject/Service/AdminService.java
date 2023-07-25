1f602f454103d5d8dcc1b0d2480d56c36796f3d4
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-25 20:03:02 +0100

    Making it more dynamic

--- src/main/java/com/project1/libaryproject/Service/AdminService.java
+++ src/main/java/com/project1/libaryproject/Service/AdminService.java
@@ -1,9 +1,16 @@
 package com.project1.libaryproject.Service;
 
+import com.project1.libaryproject.Repository.BookRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@AllArgsConstructor
 @Transactional
 public class AdminService {
+
+    private BookRepository bookRepository;
+
+
 }
