0cef145eadf9afb5083fbaab66d29cf83c077e45
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-27 01:47:56 +0100

    Making it more dynamic

--- src/main/java/com/project1/libaryproject/Repository/CheckOutRepository.java
+++ src/main/java/com/project1/libaryproject/Repository/CheckOutRepository.java
@@ -2,6 +2,7 @@ package com.project1.libaryproject.Repository;
 
 import com.project1.libaryproject.Entity.Checkout;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,4 +11,6 @@ public interface CheckOutRepository extends JpaRepository<Checkout, Long> {
     Checkout findByUserEmailAndBookId(String UserEmail, Long BookId);
 
     List<Checkout> findByUserEmail(String UserEmail);
+
+    void deleteAllByBookId(@Param("book_id") Long bookId);
 }
