4d2f2252e9699835c4aeb1229c6c5f41d978a12c
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-27 01:56:00 +0100

    Making it more dynamic

--- src/main/java/com/project1/libaryproject/Service/AdminService.java
+++ src/main/java/com/project1/libaryproject/Service/AdminService.java
@@ -19,35 +19,41 @@ public class AdminService {
     private ReviewRepository reviewRepository;
 
 
-public void postBook(AddBookRequest addBookRequest) {
-    Book book = new Book();
-    book.setTitle(addBookRequest.getTitle());
-    book.setAuthor(addBookRequest.getAuthor());
-    book.setDescription(addBookRequest.getDescription());
-    book.setAvailable_copies(addBookRequest.getCopies());
-    book.setCopies(addBookRequest.getCopies());
-    book.setImage(addBookRequest.getImg());
-    book.setCategory(addBookRequest.getCategory());
-    bookRepository.save(book);
+    public void postBook(AddBookRequest addBookRequest) {
+        Book book = new Book();
+        book.setTitle(addBookRequest.getTitle());
+        book.setAuthor(addBookRequest.getAuthor());
+        book.setDescription(addBookRequest.getDescription());
+        book.setAvailable_copies(addBookRequest.getCopies());
+        book.setCopies(addBookRequest.getCopies());
+        book.setImage(addBookRequest.getImg());
+        book.setCategory(addBookRequest.getCategory());
+        bookRepository.save(book);
 
-}
-//This is to let people know that there are more books available
-public void IncreaseBookQuantity(Long bookId) throws Exception {
-    //If a book can't be found, then we share an error
-    Book book = bookRepository.findById(bookId).orElseThrow(() -> new Exception("Book not found"));
-    book.setCopies(book.getCopies() + 1);
-    book.setAvailable_copies(book.getAvailable_copies() + 1);
-    bookRepository.save(book);
-}
-public void DecreaseBookQuantity(Long bookID) throws Exception{
-    Book book = bookRepository.findById(bookID).orElseThrow(() -> new Exception("Book not found"));
-    if (book.getCopies() == 0 || book.getAvailable_copies() == 0) {
-        throw new Exception("Quantity is locked");
     }
 
-    book.setCopies(book.getCopies() - 1);
-    book.setAvailable_copies(book.getAvailable_copies() - 1);
-}
+    //This is to let people know that there are more books available
+    public void IncreaseBookQuantity(Long bookId) throws Exception {
+        //If a book can't be found, then we share an error
+        Book book = bookRepository.findById(bookId).orElseThrow(() -> new Exception("Book not found"));
+        book.setCopies(book.getCopies() + 1);
+        book.setAvailable_copies(book.getAvailable_copies() + 1);
+        bookRepository.save(book);
+    }
 
+    public void DecreaseBookQuantity(Long bookID) throws Exception {
+        Book book = bookRepository.findById(bookID).orElseThrow(() -> new Exception("Book not found"));
+        if (book.getCopies() == 0 || book.getAvailable_copies() == 0) {
+            throw new Exception("Quantity is locked");
+        }
 
+        book.setCopies(book.getCopies() - 1);
+        book.setAvailable_copies(book.getAvailable_copies() - 1);
+    }
+
+    public void DeleteBook(Long bookId) throws Exception {
+        Book book = bookRepository.findById(bookId).orElseThrow(() -> new Exception("Book not found"));
+        bookRepository.delete(book);
+
+    }
 }
