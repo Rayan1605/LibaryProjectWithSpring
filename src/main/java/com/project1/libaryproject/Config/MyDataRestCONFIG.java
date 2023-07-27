ff769d2e35d121f8a6e2b1160ef216457dfcfc1f
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-27 02:03:25 +0100

    Making it more dynamic

--- src/main/java/com/project1/libaryproject/Config/MyDataRestCONFIG.java
+++ src/main/java/com/project1/libaryproject/Config/MyDataRestCONFIG.java
@@ -9,31 +9,42 @@ import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+// Enable exposing entity ids in API responses
 @Configuration
 public class MyDataRestCONFIG implements RepositoryRestConfigurer {
 
-    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry registry){
-    HttpMethod[] theUnsupportedActions = {HttpMethod.POST, HttpMethod.PATCH, HttpMethod.DELETE,HttpMethod.PUT};
+    // Method to configure RepositoryRestConfiguration
+    public void configureRepositoryRestConfiguration(
+            RepositoryRestConfiguration config, CorsRegistry registry) {
 
-    config.exposeIdsFor(Book.class);
-    config.exposeIdsFor(Review.class);
-    config.exposeIdsFor(Message.class);
+        // Array of HTTP methods to disable
+        HttpMethod[] theUnsupportedActions = {HttpMethod.POST, HttpMethod.PATCH, HttpMethod.DELETE,HttpMethod.PUT};
 
-    disableHttpMethods(config, theUnsupportedActions);
-    String theAllowedOrgin = "https://localhost:3000";
-    registry.addMapping(config.getBasePath() + "/**").allowedOrigins(theAllowedOrgin);
-}
+        // Expose ids in API responses
+        config.exposeIdsFor(Book.class);
+        config.exposeIdsFor(Review.class);
+        config.exposeIdsFor(Message.class);
 
+        // Disable specified HTTP methods
+        disableHttpMethods(config, theUnsupportedActions);
+
+        // Allow API requests from this origin
+        String theAllowedOrigin = "https://localhost:3000";
+
+        // Add CORS mapping
+        registry.addMapping(config.getBasePath() + "/**")
+                .allowedOrigins(theAllowedOrigin);
+    }
+
+    // Helper method to disable HTTP methods
     private void disableHttpMethods(RepositoryRestConfiguration config,
                                     HttpMethod[] theUnsupportedActions) {
 
-    config.getExposureConfiguration().forDomainType(Book.class).withItemExposure((metdata, httpMethods)
-                    -> httpMethods.disable(theUnsupportedActions))
-            .withCollectionExposure((metdata, httpMethods)
-                    -> httpMethods.disable(theUnsupportedActions));
-
-
+        // Disable methods for Book resources
+        config.getExposureConfiguration()
+                .forDomainType(Book.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
     }
-
 
 }
