dd28d480ebbaa0274365a9e1b0226029392684fa
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-08-03 03:45:52 +0100

    Making it more dynamic

--- src/main/java/com/project1/libaryproject/Controller/PaymentController.java
+++ src/main/java/com/project1/libaryproject/Controller/PaymentController.java
@@ -1,4 +1,10 @@
 package com.project1.libaryproject.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = "http://localhost:3000")
 public class PaymentController {
+
+
 }
