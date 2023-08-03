95f03d4990ea7ed42641ddf321668d6feb27b44a
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-08-03 03:01:24 +0100

    Making it more dynamic

--- src/main/java/com/project1/libaryproject/Entity/Payment.java
+++ src/main/java/com/project1/libaryproject/Entity/Payment.java
@@ -1,4 +1,11 @@
 package com.project1.libaryproject.Entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "payment")
 public class Payment {
 }
