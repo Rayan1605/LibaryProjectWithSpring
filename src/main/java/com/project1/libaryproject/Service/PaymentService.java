a418a56bc3e781dfca79b2a0f669d7aba9a2a320
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-08-03 03:15:53 +0100

    Making it more dynamic

--- src/main/java/com/project1/libaryproject/Service/PaymentService.java
+++ src/main/java/com/project1/libaryproject/Service/PaymentService.java
@@ -1,4 +1,11 @@
 package com.project1.libaryproject.Service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
 public class PaymentService {
+
+
 }
