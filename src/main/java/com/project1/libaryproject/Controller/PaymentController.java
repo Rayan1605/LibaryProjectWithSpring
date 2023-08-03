103fb5742af20188454b98805d28abfe33b888d2
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-08-03 03:48:39 +0100

    Making it more dynamic

--- src/main/java/com/project1/libaryproject/Controller/PaymentController.java
+++ src/main/java/com/project1/libaryproject/Controller/PaymentController.java
@@ -1,11 +1,11 @@
 package com.project1.libaryproject.Controller;
 
+import com.project1.libaryproject.RequestModels.PaymentInfoRequest;
 import com.project1.libaryproject.Service.PaymentService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -14,5 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class PaymentController {
 
     private PaymentService paymentService;
+
+    @PostMapping("/payment-intent")
+    public ResponseEntity<String> createPaymentIntent(@RequestBody PaymentInfoRequest paymentInfoRequest) throws Exception {
+        return ResponseEntity.ok(paymentService.createPaymentIntent(paymentInfoRequest));
+    }
 
 }
