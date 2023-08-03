7a3ce1d63219f269bed92118c472717387724c10
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-08-03 03:18:14 +0100

    Making it more dynamic

--- src/main/java/com/project1/libaryproject/Service/PaymentService.java
+++ src/main/java/com/project1/libaryproject/Service/PaymentService.java
@@ -1,15 +1,23 @@
 package com.project1.libaryproject.Service;
 
 import com.project1.libaryproject.Repository.PaymentRepository;
+import com.stripe.Stripe;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
-@AllArgsConstructor
 public class PaymentService {
     private PaymentRepository paymentRepository;
-
+@Autowired
+    public PaymentService(PaymentRepository paymentRepository,
+                          // Getting the secret key from the application.properties file
+                          @Value("${stripe.key.secret}") String secretKey) {
+        this.paymentRepository = paymentRepository;
+        Stripe.apiKey = secretKey;
+    }
 
 }
