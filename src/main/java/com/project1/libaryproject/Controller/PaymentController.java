0daf272a83b62c7f6bfca2d5092822e8b66bfa47
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-08-03 03:47:15 +0100

    Making it more dynamic

--- src/main/java/com/project1/libaryproject/Controller/PaymentController.java
+++ src/main/java/com/project1/libaryproject/Controller/PaymentController.java
@@ -1,5 +1,7 @@
 package com.project1.libaryproject.Controller;
 
+import com.project1.libaryproject.Service.PaymentService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,8 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
+@AllArgsConstructor
 @RequestMapping("/api/payment/secure")
 public class PaymentController {
 
+    private PaymentService paymentService;
 
 }
