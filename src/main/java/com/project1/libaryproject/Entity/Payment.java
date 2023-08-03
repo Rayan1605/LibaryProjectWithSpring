8ab502b919daae78420f266042640218b7f4a959
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-08-03 03:02:54 +0100

    Making it more dynamic

--- src/main/java/com/project1/libaryproject/Entity/Payment.java
+++ src/main/java/com/project1/libaryproject/Entity/Payment.java
@@ -1,6 +1,8 @@
 package com.project1.libaryproject.Entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -8,4 +10,8 @@ import lombok.Data;
 @Entity
 @Table(name = "payment")
 public class Payment {
+   @Id
+    private Long id;
+
+   private String useremail;
 }
