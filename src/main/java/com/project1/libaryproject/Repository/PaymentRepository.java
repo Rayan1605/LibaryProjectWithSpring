ae721a6b79b355b104d847e7ac3f73b709a8b6d6
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-08-03 03:07:43 +0100

    Making it more dynamic

--- src/main/java/com/project1/libaryproject/Repository/PaymentRepository.java
+++ src/main/java/com/project1/libaryproject/Repository/PaymentRepository.java
@@ -4,4 +4,5 @@ import com.project1.libaryproject.Entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PaymentRepository extends JpaRepository<Payment ,Long> {
+    Payment findByUserEmail(String userEmail);
 }
